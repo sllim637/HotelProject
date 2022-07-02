79cbdbdf6b904a83f60218db97df9346f100d443
sllim637 <slim.hammami1977@gmail.com>
2022-07-03 00:18:42 +0200

    adding IUnit of work and UnitOfWorkInterface

---
+++ tutorialProject/IRepository/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using tutorialProject.Models;
+
+namespace tutorialProject.IRepository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<Country> Countries{get;}
+        IGenericRepository<Hotel> Hotels { get; }
+        Task Save();
+
+    }
+}
---
+++ tutorialProject/Repository/UnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using tutorialProject.Data;
+using tutorialProject.IRepository;
+using tutorialProject.Models;
+
+namespace tutorialProject.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    { 
+        private readonly DatabaseContext _context;
+        private IGenericRepository<Country> _countries;
+        private IGenericRepository<Hotel> _hotels;
+        public UnitOfWork( DatabaseContext context)
+        {
+            _context = context;
+        }
+        public IGenericRepository<Country> Countries => _countries ??= new GenericRepository<Country>(_context);
+
+        public IGenericRepository<Hotel> Hotels => _hotels ??= new GenericRepository<Hotel>(_context);
+        // this method is like the garbage collector wehn operations is done free the memory
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
